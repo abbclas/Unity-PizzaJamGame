9099a6631d0ed0efa7b7ce6a95e7ce33d982ab84
AMIRALI <sharifiamirali3@gmail.com>
2023-07-22 01:31:11 +0330

    ItemSO

---
+++ PizzaJamGame/Assets/Scripts/ScriptableObjectsScripts/ItemSO.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "ItemSO", menuName = "ItemsSO/ItemSO")]
+public class ItemSO : ScriptableObject
+{
+    public string _ItemName;
+    public int _ItemID;
+    public int _StackNum;
+    //////////////////////////////
+    public ItemType Type;
+
+    public enum ItemType {
+        Healing,
+        Damaging,
+        Loot,
+        BuldingMaterial,
+        Tools
+    };
+}
